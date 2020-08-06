be0bcbef2515ee7009d3224214c87242e309109d
xander557 <yikanwang557@gmail.com>
2020-08-05 21:17:52 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
